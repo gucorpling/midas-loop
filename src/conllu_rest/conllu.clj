1bac827aed66f1b8cab0b41291b877b88675f944
Luke Gessler <lukegessler@gmail.com>
2021-11-28 19:37:21 -0500

    add native parser

--- src/conllu_rest/conllu.clj
+++ src/conllu_rest/conllu.clj
@@ -1,7 +1,8 @@
 (ns conllu-rest.conllu
   "Wrapper for Python conllu library"
   (:require [libpython-clj.python :refer [py. py.. py.-] :as py]
-            [libpython-clj.require :refer [require-python]])
+            [libpython-clj.require :refer [require-python]]
+            [clojure.string :as string])
 
   )
 
@@ -13,10 +14,10 @@
 (require-python '[conllu :as conllu])
 (require-python '[builtins :as python])
 
-(defn parse-conllu-string [conllu-string]
+(defn parse-conllu-string-with-python [conllu-string]
   (mapv (fn [token-list]
           {:tokens        (mapv (fn [token]
-                                  (-> (into {} (into {} (map (fn [[k v]] [(keyword k) v]) (python/dict token))) )
+                                  (-> (into {} (into {} (map (fn [[k v]] [(keyword k) v]) (python/dict token))))
                                       (update :feats #(try (into {} (python/dict %)) (catch Exception e {})))
                                       (update :deps #(try (into {} (python/dict %)) (catch Exception e {})))
                                       (update :misc #(try (into {} (python/dict %)) (catch Exception e {})))
@@ -26,22 +27,97 @@
            :python-object token-list})
         (conllu/parse conllu-string)))
 
+;; Native implementation
+(defn metadata-line? [line]
+  (= \# (get (string/trim line) 0)))
+
+(defn parse-metadata-line [line]
+  (let [line (string/trim line)
+        index (string/index-of line "=")]
+    ;; Be forgiving and just fail silently if we can't find a "="
+    (when (not (or (nil? index) (= (inc index) (count line))))
+      (let [key (string/trim (subs line 1 index))
+            value (string/trim (subs line (inc index)))]
+        [key value]))))
+
+(def super-token-re #"(\d+)(-)(\d+)")
+(def empty-token-re #"(\d+)(\.)(\d+)")
+
+(defn get-groups [re v]
+  (let [matcher (re-matcher re v)]
+    (when (re-find matcher)
+      (let [[_ n1 sep n2] (re-groups matcher)]
+        [(Long/parseLong n1) sep (Long/parseLong n2)]))))
+
+(defn parse-id
+  [v]
+  (let [v (string/trim v)
+        groups (or (get-groups super-token-re v)
+                   (get-groups empty-token-re v))]
+    (if groups
+      groups
+      (Long/parseLong v))))
+
+(defn parse-atomic [v] (if (= v "_") nil v))
+
+(defn parse-assoc
+  ([v] (parse-assoc "=" v))
+  ([kv-sep v]
+   (if (= v "_")
+     []
+     (let [kv-pairs (string/split v #"\|")]
+       (into {}
+             (for [pair kv-pairs]
+               (let [index (string/index-of pair kv-sep)
+                     key (subs pair 0 index)
+                     value (subs pair (inc index))]
+                 [key value])))))))
+
+(defn parse-token-line [line]
+  (let [cols (string/split (string/trim line) #"\t")]
+    (when-not (= (count cols) 10)
+      (throw (ex-info (str "Expected 10 columns in token, found " (count cols)) {:actual (count cols)
+                                                                                 :cols   cols})))
+    {:id     (parse-id (get cols 0))
+     :form   (get cols 1)
+     :lemma  (get cols 2)
+     :upos   (parse-atomic (get cols 3))
+     :xpos   (parse-atomic (get cols 4))
+     :feats  (parse-assoc (get cols 5))
+     :head   (parse-atomic (get cols 6))
+     :deprel (parse-atomic (get cols 7))
+     :deps   (parse-assoc ":" (get cols 8))
+     :misc   (parse-assoc (get cols 9))}))
+
+(defn parse-sentence-lines [lines]
+  (let [lines (string/split-lines lines)
+        metadata-lines (filter metadata-line? lines)
+        token-lines (filter #(not (metadata-line? %)) lines)]
+    {:metadata (into {} (map parse-metadata-line metadata-lines))
+     :tokens   (into [] (map parse-token-line token-lines))}))
+
+(defn parse-conllu-string [conllu-string]
+  (map parse-sentence-lines (string/split (string/trim conllu-string) #"\n\n")))
+
 (comment
-  (def data "
-# text = The quick brown fox jumps over the lazy dog.
-1   The     the    DET    DT   Definite=Def|PronType=Art   4   det     _   _
-2   quick   quick  ADJ    JJ   Degree=Pos                  4   amod    _   _
-3   brown   brown  ADJ    JJ   Degree=Pos                  4   amod    _   _
-4   fox     fox    NOUN   NN   Number=Sing                 5   nsubj   _   _
-5   jumps   jump   VERB   VBZ  Mood=Ind|Number=Sing|Person=3|Tense=Pres|VerbForm=Fin   0   root    _   _
-6   over    over   ADP    IN   _                           9   case    _   _
-7   the     the    DET    DT   Definite=Def|PronType=Art   9   det     _   _
-8   lazy    lazy   ADJ    JJ   Degree=Pos                  9   amod    _   _
-9   dog     dog    NOUN   NN   Number=Sing                 5   nmod    _   SpaceAfter=No
-10  .       .      PUNCT  .    _                           5   punct   _   _
-11.1  empty       _      _  _    _                           _   _   _   _
+
+  (def data "# s_type = decl
+# sent_id = AMALGUM_bio_bacon-4
+# text = He and his band were relentlessly pursued thereafter .
+1	He	he	PRON	PRP	Case=Nom|Gender=Masc|Number=Sing|Person=3|PronType=Prs	7	nsubj:pass	7:nsubj:pass	Discourse=joint:13->3|Entity=(person-1)
+2	and	and	CCONJ	CC	_	4	cc	4:cc	_
+3	his	his	PRON	PRP$	Gender=Masc|Number=Sing|Person=3|Poss=Yes|PronType=Prs	4	nmod:poss	4:nmod:poss	Entity=(organization-6(person-1)
+4	band	band	NOUN	NN	Number=Sing	1	conj	1:conj:and|7:nsubj:pass	Entity=organization-6)
+5	were	be	AUX	VBD	Mood=Ind|Number=Plur|Person=3|Tense=Past|VerbForm=Fin	7	aux:pass	7:aux:pass	_
+6	relentlessly	relentlessly	ADV	RB	Degree=Pos	7	advmod	7:advmod	_
+7	pursued	pursue	VERB	VBN	Tense=Past|VerbForm=Part|Voice=Pass	0	root	0:root	_
+8	thereafter	thereafter	ADV	RB	Degree=Pos	7	advmod	7:advmod	_
+9	.	.	PUNCT	.	_	7	punct	7:punct	_
 
 ")
+
+  (def xs (parse-conllu-string-with-python data))
+
 
   (def xs (parse-conllu-string data))
 
