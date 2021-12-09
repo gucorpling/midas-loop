1e0f9f4d8e9d49c38e89766233afcaa5c257a294
Luke Gessler <lukegessler@gmail.com>
2021-12-09 18:02:28 -0500

    add nyi and uuid-string?

--- src/conllu_rest/common.clj
+++ src/conllu_rest/common.clj
@@ -1,5 +1,15 @@
-(ns conllu-rest.common)
+(ns conllu-rest.common
+  (:import (java.util UUID)))
+
+(defn uuid-string? [s]
+  (try (do (UUID/fromString s) true)
+       (catch IllegalArgumentException _
+         false)))
 
 (defn error-response [error-details]
   {:status (:status error-details)
    :body   error-details})
+
+(defn nyi-response [request]
+  {:status 501
+   :body   "Not implemented"})
