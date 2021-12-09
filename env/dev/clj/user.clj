6df9a34dcd1ed764661824e317ed7b71586f2135
Luke Gessler <lukegessler@gmail.com>
2021-12-09 18:01:52 -0500

    misc improvements

--- env/dev/clj/user.clj
+++ env/dev/clj/user.clj
@@ -1,10 +1,12 @@
 (ns user
   "Userspace functions you can run by default in your local REPL."
-  (:require [conllu-rest.server.config :refer [env]]
+  (:require [clojure.tools.namespace.repl :as tools-ns]
+            [conllu-rest.server.config :refer [env]]
             [clojure.pprint]
             [clojure.spec.alpha :as s]
             [expound.alpha :as expound]
             [mount.core :as mount]
+            [conllu-rest.xtdb.easy :as cxe]
             [conllu-rest.core :refer [start-app]]))
 
 (alter-var-root #'s/*explain-out* (constantly expound/printer))
@@ -15,6 +17,7 @@
   "Starts application.
   You'll usually want to run this on startup."
   []
+  (def node conllu-rest.server.xtdb/xtdb-node)
   (mount/start-without #'conllu-rest.core/repl-server))
 
 (defn stop
@@ -26,6 +29,6 @@
   "Restarts application."
   []
   (stop)
-  (start))
+  (tools-ns/refresh :after 'user/start))
 
 
