0154af231ab71e96e4acdcb474b111a5101268c2
Luke Gessler <lukegessler@gmail.com>
2022-06-23 22:02:19 -0400

    fix link

--- scripts/midas-loop.sh
+++ scripts/midas-loop.sh
@@ -15,7 +15,7 @@ trap cleanup EXIT
 # Check existence of files
 if [[ ! -f "midas-loop.jar" ]]; then 
   echo "Did not find midas-loop.jar. Downloading at ./midas-loop.jar"
-  curl -L "https://github.com/gucorpling/midas-loop/releases/download/v0.0.1/midas-loop-v0.0.1.jar" -o "midas-loop.jar"
+  curl -L "https://github.com/gucorpling/midas-loop/releases/download/v0.0.1/midas-loop.jar" -o "midas-loop.jar"
 fi
 if [[ ! -f "config.edn" ]]; then 
   echo "Did not find config.edn. Downloading at ./config.edn"
